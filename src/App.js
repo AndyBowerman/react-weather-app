2af57a3c5ca0e42e55f8f372d06d1f0c7b55a30d
AndyBowerman <andy_bowerman06@hotmail.com>
2022-02-17 14:56:49 +0000

    first commit - CSS styling applied

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+const api = {
+  key: 'c9fc4bc2ae12b6899ceb97c1633ae166',
+  base: 'https://api.openweathermap.org/data/2.5/'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+          />
+        </div>
+        <div className="location-box">
+          <div className="location">New York City</div>
+          <div className="date"></div>
+        </div>
+      </main>
     </div>
   );
 }
